701e44610e66441df0d34c8912ce84f549a83654
Geert Guldentops <geert.guldentops@ipc.be>
2018-09-24 20:07:47 +0200

    Pulled a semantic-analyser module out of the main jlox module.

--- semantic-analyser/src/main/java/be/guldentops/geert/lox/semantic/analysis/Resolver.java
+++ semantic-analyser/src/main/java/be/guldentops/geert/lox/semantic/analysis/Resolver.java
@@ -2,14 +2,13 @@ package be.guldentops.geert.lox.semantic.analysis;
 
 import be.guldentops.geert.lox.error.CanReportErrors;
 import be.guldentops.geert.lox.grammar.Statement;
-import be.guldentops.geert.lox.interpreter.Interpreter;
 
 import java.util.List;
 
 public interface Resolver extends CanReportErrors {
 
-    static Resolver createDefault(Interpreter interpreter) {
-        return new VariableResolver(interpreter);
+    static Resolver createDefault(ResolutionAnalyzer resolutionAnalyzer) {
+        return new VariableResolver(resolutionAnalyzer);
     }
 
     void resolve(List<Statement> statements);
