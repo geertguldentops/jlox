6a805d800a0df5abf664bcddc928194cf5838452
Geert Guldentops <geert.guldentops@corsearch.com>
2019-05-21 07:54:44 +0200

    Change the target Java version to JDK 12.

--- cleanInstall.sh
+++ cleanInstall.sh
@@ -4,10 +4,15 @@ function setMavenOpts() {
 	export MAVEN_OPTS='-Xms4096m -Xmx4096m -XX:+CMSClassUnloadingEnabled'
 }
 
+function downgradeToJDK12() {
+	export JAVA_HOME=`/usr/libexec/java_home -v 12`
+}
+
 function doCleanInstall() {
 	mvn clean install
 }
 
 setMavenOpts
+downgradeToJDK12
 
 doCleanInstall
